c0e469bea1b46e1423457ea53d91b9f23b5e5b11
gitDemo <nitinbone5225@gmail.com>
2025-01-27 15:03:46 +0530

    Your commit message

---
+++ src/app/Model/to-do.ts
@@ -0,0 +1,4 @@
+export interface ToDoModel {
+    id?:number;
+    addTodo:string;
+}
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,33 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ToDoModel } from './Model/to-do';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  private apiUrl ='http://localhost:8080'; 
+  private apiUrl="http://localhost:8080";
 
   constructor(private http: HttpClient) { } //  allows you to make HTTP requests
 
-  getTodos(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/allList`);
+
+
+  getTodo():Observable<ToDoModel[]>{
+    return this.http.get<ToDoModel[]>(`${this.apiUrl}/allList`);
   }
 
-  // POST: Add a new To-Do item
-  addTodo(todo: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/newTodo`, todo);
+  addTodo(toDo: ToDoModel): Observable<ToDoModel> {
+    return this.http.post<ToDoModel>(`${this.apiUrl}/newTodo`, toDo);
   }
 
-  // PUT: Update a specific To-Do item
-  updateTodo(id: number, todo: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/update/${id}`, todo);
+  updateTodo(toDo:ToDoModel):Observable<ToDoModel>{
+    return this.http.put<ToDoModel>(`${this.apiUrl}/update/${toDo.id}`,toDo)
   }
 
-  // DELETE: Delete a specific To-Do item
-  deleteTodo(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/delete/${id}`);
+  deleteTodo(id: number): Observable<string> {
+    return this.http.delete<string>(`${this.apiUrl}/delete/${id}`, { responseType: 'text' as 'json' });
   }
 
 
